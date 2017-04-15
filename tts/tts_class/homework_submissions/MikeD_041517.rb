25c280091982f0af0a20422c889595d5a7c5adc3
Mike DeBerry <mik3.de1@gmail.com>
2017-04-15 08:10:34 -0400

    homework_git_test

---
+++ tts/tts_class/homework_submissions/MikeD_041517.rb
@@ -0,0 +1,7 @@
+Students = []
+Teachers = []
+	puts "If you are a student enter your first and last name IE: John Smith or Jane Smith"
+	puts "IF you are a teacher enter your Honoric and last name IE: Mr/Mrs/Miss.Smith"
+def type(name)
+	
+end
